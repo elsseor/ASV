de31a37c477bd02f72f87a6a4345278fd75a1849
elsseor <63708273+elsseor@users.noreply.github.com>
2023-03-10 10:59:27 +0100

    Update createMailList.ps1
    Berücksichtigt auch eine Schulerliste

--- Mailinglisten/createMailList.ps1
+++ Mailinglisten/createMailList.ps1
@@ -1,12 +1,12 @@
 ############################################# Einstellungen
 # Setzen Sie $MailAnspPa auf $true, wenn auch die Emailadressen der
 # Ansprechpartner in die Exceldatei übernommen werden sollen
-$MailAnspPa = $false
+$MailAnspPa = $true
 # $MailAnspPa = $true
 
-$filename = Betriebe.xlsx
 
 ############################################# Programm
+# Prüfen ob Betriebe.xlsx und schueler.xlsx im richtigen Format vorliegt
 
 
 clear-host
@@ -23,7 +23,57 @@ if (-not(Test-path -path $skriptpath\Betriebe.xlsx)){
     exit
 }
 
-$file = Import-Excel -path $skriptpath\Betriebe.xlsx
+
+if ((Test-path -path $skriptpath\schueler.xls) -and -not(Test-path -path $skriptpath\schueler.xlsx)){
+    Write-host Es ist eine "schueler.xls" vorhanden, die nicht im aktuellen Excelformat vorliegt
+    Write-Host Diese Datei kann nicht berücksichtigt werden
+    Pause
+}
+
+
+############################################# Betriebeliste einschränken
+# Falls eine schueler.xlsx vorhanden ist, wird die Liste der Betriebe so eingeschränkt,
+# dass nur die Betriebe dieser Schüler berücksichtigt werden
+
+
+
+$betriebe = @()
+if (Test-path -path $skriptpath\schueler.xlsx ){
+    $file1 = Import-Excel -path $skriptpath\schueler.xlsx
+    foreach ($i in $file1){
+        if (-not($betriebe.Contains($i.'Ausb. Betrieb Name1'))){
+            $betriebe += $i.'Ausb. Betrieb Name1'
+        }
+    }
+
+
+
+    $file2 = Import-Excel -path $skriptpath\betriebe.xlsx
+    $zeile = @()
+    foreach ($i in $file2){
+        if ($betriebe.Contains($i.'Betriebename Zeile 1')){
+            $zeile += $i
+        }
+    }
+
+    $zeile | Export-Excel -path $skriptpath\betriebetemp.xlsx
+
+}
+else{
+    copy-Item $skriptpath\betriebe.xlsx $skriptpath\betriebetemp.xlsx
+}
+
+
+
+
+
+
+
+
+
+
+############################################# Mailadressen exportieren
+$file = Import-Excel -path $skriptpath\betriebetemp.xlsx
 $liste = @()
 $allmail = @()
 
@@ -76,3 +126,4 @@ foreach ($i in $file) {
 
 ##### Über Select-Object kann festgelegt, welche Spalten in die neue Exceldatei aufgenommen wird und in welcher Reihenfolge
 $liste | Select-Object -Property 'Betriebename Zeile 1','Anschrift E-Mail' | Export-Excel -path $skriptpath\mail.xlsx
+Remove-Item $skriptpath\betriebetemp.xlsx
